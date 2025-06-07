4e6258a5bc42b199b0b152bc14d3f16f159d2f8d
Kasim Tasdemir <kasimtasdemir@gmail.com>
2025-06-08 00:51:19 +0100

    style: format fixed

--- src/pudding/__init__.py
+++ src/pudding/__init__.py
@@ -1,15 +1,15 @@
 """Pudding - The sweetest way to build data pipelines"""
 
 from .core.base_component import BaseComponent
+from .core.config import RunConfig, SampleSaveMode
 from .core.data_envelope import DataEnvelope, DataType
 from .core.registry import ComponentRegistry, register_component
-from .core.config import RunConfig, SampleSaveMode
 
 __version__ = "0.1.0"
 __all__ = [
     "BaseComponent",
     "DataEnvelope",
-    "DataType", 
+    "DataType",
     "ComponentRegistry",
     "register_component",
     "RunConfig",
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,11 +1,12 @@
 """Basic tests to get CI passing."""
 
+
 def test_import():
     """Test that we can import the main module."""
     # Simple import test - adjust based on your actual structure
     assert True  # Placeholder until you have real code to test
 
+
 def test_placeholder():
     """Placeholder test."""
     assert 1 + 1 == 2
-
